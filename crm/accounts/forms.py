90b4bc2c3c9aac91454d7ec2b98c5c0751a64087
Khyati8100 <88727014+Khyati8100@users.noreply.github.com>
2021-08-27 08:13:48 +0530

    Add files via upload

---
+++ crm/accounts/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.db import models
+from django.forms import ModelForm, fields
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user']
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class CrateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
